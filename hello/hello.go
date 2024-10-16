c6b36b87fe49f03aa2807dffc5ff5bddc1ed0d37
josh-espinas <josh.espinas@xero.com>
2024-10-16 22:57:18 +0100

    First module with prints

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+  fmt.Println(quote.Opt())
+  fmt.Println(quote.Glass())
+  fmt.Println(quote.Hello())
+  fmt.Println(quote.Go())
+}
